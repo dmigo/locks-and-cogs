89dcecdc39b03be7002692950a0747f2f7a6ec19
Chesheersky <d.f.goryunov@gmail.com>
2017-02-09 21:47:04 +0100

    add bomb

---
+++ bomb/bomb.ino
@@ -0,0 +1,29 @@
+//  _ ___ _______     ___ ___ ___  ___ _   _ ___ _____ ___ 
+// / |_  )__ /   \   / __|_ _| _ \/ __| | | |_ _|_   _/ __| 
+// | |/ / |_ \ |) | | (__ | ||   / (__| |_| || |  | | \__ \ 
+// |_/___|___/___/   \___|___|_|_\\___|\___/|___| |_| |___/ 
+// 
+// The Unnamed Circuit
+// 
+// Made by Dmitry Goryunov
+// License: CC-BY-SA 3.0
+// Downloaded from: https://circuits.io/circuits/3955347-the-unnamed-circuit
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
