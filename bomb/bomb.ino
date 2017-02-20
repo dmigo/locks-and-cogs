18b7d50d78e8feaaeab9009faec021235cb6e6c7
DGO <d.f.goryunov@gmail.com>
2017-02-20 23:53:53 +0100

    add win and loose melodies

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -7,7 +7,103 @@
 // 
 // Made by Dmitry Goryunov
 // License: CC-BY-SA 3.0
-// Downloaded from: https://circuits.io/circuits/3955347-ma-baby
+// Downloaded from: https://circuits.io/circuits/4065601-ma-baby
+
+/*************************************************
+ * Notes
+ *************************************************/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+#define NOTE_SUSTAIN 250
 
 #include <LiquidCrystal.h>
 #include <Keypad.h>
@@ -27,7 +123,7 @@ byte rowPins[numRows] = {6,7,8,9};
 byte colPins[numCols]= {A0,A1,A2,A3}; 
 
 const int totalTime = 3600; // in decisecs
-int time = 0;
+int time = totalTime;
 bool tick = false;
 
 //3600, 3300, 3000, 2700, 2400, 2100, 1800, 1500, 1200, 900, 600, 300, 30, 20, 10
@@ -152,10 +248,41 @@ void printTime(){
 }
 
 void win(){
-  //todo play melody
+  
   disarmed = true;
+  
+  tone(buzzerPin,NOTE_A5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_B5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_C5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_B5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_C5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_D5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_C5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_D5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_E5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_D5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_E5);
+  delay(NOTE_SUSTAIN);
+  tone(buzzerPin,NOTE_E5);
+  delay(NOTE_SUSTAIN);
+  noTone(buzzerPin);
 }
 void loose(){
-  //todo play melody
   exploded = true;
+
+  tone(buzzerPin,NOTE_G4);
+  delay(250);
+  tone(buzzerPin,NOTE_C4);
+  delay(500);
+  noTone(buzzerPin);
 }
