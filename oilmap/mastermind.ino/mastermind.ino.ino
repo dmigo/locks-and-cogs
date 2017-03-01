a2e47394e27f3e74d603e694d1f0dab212b5d548
DGO <d.f.goryunov@gmail.com>
2017-03-01 23:25:15 +0100

    let it tick

--- oilmap/mastermind.ino/mastermind.ino.ino
+++ oilmap/mastermind.ino/mastermind.ino.ino
@@ -5,12 +5,37 @@
 #define CAPACITY 1 
 
 #define IS_ACTIVE 0 
-#define ADDRESS 1 
-#define SPEED 2 
-#define FROM 3 
-#define TO 1
+#define ADDRESS 1  
+#define ADDRESS_ADD 2 
+#define SPEED 3 
+#define FROM 4 
+#define TO 5
+
+#define WELLID 0
+#define MARKETID 1
+#define STORAGEID 2
+
+#define DAY 2
 
 #include <Wire.h>
+
+//{wellId, marketId, storageId,}
+int nodes [][] = {
+  {-1, -1, 0},
+  {-1, -1, 1},
+  {-1, -1, 2},
+};
+
+int wells [][] ={
+  {},
+  {},  
+};
+
+int markets [][] ={
+  {},
+  {},  
+};
+
 //{content, capacity}
 int storages [3][2] ={
   {300, 300},
@@ -19,40 +44,75 @@ int storages [3][2] ={
 };
 
 //{isActive, address, speed, from, to}
-int pipes [2][5] ={
-  {INACTIVE, 7, 2000, 0, 1},
-  {INACTIVE, 8, 500, 1, 2}
+int pipesCount = 2;
+int pipes [pipesCount][5] ={
+  {INACTIVE, 7, 0, 2000, 0, 1},
+  {INACTIVE, 8, 0, 500, 1, 2}
 };
 
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
 }
 
+//=== PIPES ===//
 void activatePipe(int index){
   int speed = pipes[index][SPEED];
-  pipes[index][IS_ACTIVE] = ACTIVE;
+  
+  pipes[index][IS_ACTIVE] = ACTIVE;//ToDo set -ACTIVE for reverse
+    
   Wire.beginTransmission(pipes[index][ADDRESS]);
   Wire.write(speed);
   Wire.write(speed>>8);
-  Wire.endTransmission();
+  Wire.endTransmission();  
 }
-
 
 void deactivatePipe(int index){
   pipes[index][IS_ACTIVE] = INACTIVE;
+  
   Wire.beginTransmission(pipes[index][ADDRESS]);
   Wire.write(0);
   Wire.write(0);
   Wire.endTransmission();
 }
 
-void loop() {
-  activatePipe(0);
-  delay(500);
-  activatePipe(1);
-  delay(500);
-  deactivatePipe(0);
-  delay(500);
-  deactivatePipe(1);
-  delay(500);
+void pump(int from, int to, int speed){
+  
 }
+
+//=== PIPES ===//
+
+void loop() {
+  doTick();
+  doTock();
+  delay(100); 
+}
+
+//=== TIME ===//
+int seconds = 0;
+bool tick = false;
+
+void dotick(){
+  var newTime = millis() / 1000;
+  if(newTime - seconds >= DAY)
+  {
+      tick = true;
+      seconds = newTime;
+  }
+}
+
+void doTock(){
+  if(tick){
+    for(int i = 0; i < pipesCount;
+    {
+      if(pipes[i][IS_ACTIVE] == ACTIVE)
+        pump(pipes[i][FROM],pipes[i][TO], pipes[SPEED]);
+        
+      if(pipes[i][IS_ACTIVE] == -ACTIVE)
+        pump(pipes[i][TO],pipes[i][FROM], pipes[SPEED]);
+    }
+    //ToDo do stuff
+    tick = false;
+  }
+}
+//=== TIME ===//
+
