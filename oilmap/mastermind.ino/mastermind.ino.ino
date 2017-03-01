a7823540c4b4ac59d260c219cab78b2443f86697
DGO <d.f.goryunov@gmail.com>
2017-03-01 22:43:46 +0100

    implement mastermind

--- oilmap/mastermind.ino/mastermind.ino.ino
+++ oilmap/mastermind.ino/mastermind.ino.ino
@@ -1,9 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
+#define INACTIVE 0 
+#define ACTIVE 1 
 
+#define CONTENT 0 
+#define CAPACITY 1 
+
+#define IS_ACTIVE 0 
+#define ADDRESS 1 
+#define SPEED 2 
+#define FROM 3 
+#define TO 1
+
+#include <Wire.h>
+//{content, capacity}
+int storages [3][2] ={
+  {300, 300},
+  {0, 100},
+  {50, 300},
+};
+
+//{isActive, address, speed, from, to}
+int pipes [2][5] ={
+  {INACTIVE, 7, 2000, 0, 1},
+  {INACTIVE, 8, 500, 1, 2}
+};
+
+void setup() {
+  Wire.begin(); // join i2c bus (address optional for master)
+}
+
+void activatePipe(int index){
+  int speed = pipes[index][SPEED];
+  pipes[index][IS_ACTIVE] = ACTIVE;
+  Wire.beginTransmission(pipes[index][ADDRESS]);
+  Wire.write(speed);
+  Wire.write(speed>>8);
+  Wire.endTransmission();
+}
+
+
+void deactivatePipe(int index){
+  pipes[index][IS_ACTIVE] = INACTIVE;
+  Wire.beginTransmission(pipes[index][ADDRESS]);
+  Wire.write(0);
+  Wire.write(0);
+  Wire.endTransmission();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  activatePipe(0);
+  delay(500);
+  activatePipe(1);
+  delay(500);
+  deactivatePipe(0);
+  delay(500);
+  deactivatePipe(1);
+  delay(500);
 }
