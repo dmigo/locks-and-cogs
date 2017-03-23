74a194a446a82fcbaf1a77e7e8019a925a3a7c2d
DGO <d.f.goryunov@gmail.com>
2017-03-23 13:04:47 +0100

    small fixes

--- ir-uv/ir-uv.ino
+++ ir-uv/ir-uv.ino
@@ -1,5 +1,6 @@
 
 //=== задача 1
+#define UV_DELAY 1000 //задержка ультрафиолета в миллисекундах
 const int length = 3;//количество кнопок
 int buttons[length] = {12, 9, 6};//список кнопок
 int irleds[length] = {11, 8, 5};//список ик
@@ -10,7 +11,7 @@ const int slength = 3;//длинна правильной последовате
 int sequence[slength] = {0, 1, 2};//правильная последовательность
 
 void setup() {
-  for(int i = 0;i<length;i++){
+  for(int i = 0; i<length; i++){
   	pinMode(irleds[i], OUTPUT);
   	pinMode(uvleds[i], OUTPUT);
   	pinMode(buttons[i], INPUT_PULLUP);
@@ -27,6 +28,10 @@ void setup() {
 void start(){
   Serial.println("[Starting the game]");
   index = 0;
+  
+  for(int i = 0; i<length; i++)
+    irdrop(i);
+  
   irup(sequence[index]);
 }
 
@@ -47,9 +52,9 @@ void irup(int i){
 }
 
 void uvblink(int i){
-  digitalWrite(irleds[i], HIGH);
-  delay(1000);
-  digitalWrite(irleds[i], LOW);
+  digitalWrite(uvleds[i], HIGH);
+  delay(UV_DELAY);
+  digitalWrite(uvleds[i], LOW);
 }
 
 void win(){
@@ -79,7 +84,7 @@ void loop() {
     if(index == slength)
       win();
     else
-      irup(desired);
+      irup(sequence[index]);
   }
   else if(pressed != -1){
     printState(index, desired, pressed);
