317221566ab19601b73fc39e554ecdf75d047ce4
DGO <d.f.goryunov@gmail.com>
2017-03-23 14:26:04 +0100

    second game

--- ir-uv/ir-uv.ino
+++ ir-uv/ir-uv.ino
@@ -1,99 +1,53 @@
 
 //=== задача 1
 #define UV_DELAY 1000 //задержка ультрафиолета в миллисекундах
-const int length = 3;//количество кнопок
-int buttons[length] = {12, 9, 6};//список кнопок
-int irleds[length] = {11, 8, 5};//список ик
-int uvleds[length] = {10, 7, 4};//список ультрафиолеток
 
-int index = 0;
-const int slength = 3;//длинна правильной последовательности
-int sequence[slength] = {0, 1, 2};//правильная последовательность
+bool won1 = false;
 
-void setup() {
-  for(int i = 0; i<length; i++){
-  	pinMode(irleds[i], OUTPUT);
-  	pinMode(uvleds[i], OUTPUT);
-  	pinMode(buttons[i], INPUT_PULLUP);
-  }
-  
-  Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }  
-  
-  start();
-}
+const int length1 = 3;//количество кнопок
+int buttons1[length1] = {12, 9, 6};//список кнопок
+int irleds1[length1] = {11, 8, 5};//список ик
+int uvleds1[length1] = {10, 7, 4};//список ультрафиолеток
 
-void start(){
-  Serial.println("[Starting the game]");
-  index = 0;
+int index1 = 0;
+const int slength1 = 3;//длинна правильной последовательности
+int sequence1[slength1] = {0, 1, 2};//правильная последовательность
+
+void start1(){
+  Serial.println("[Starting the first game]");
+  index1 = 0;
+  won1 = false;
   
-  for(int i = 0; i<length; i++)
+  for(int i = 0; i<length1; i++)
     irdrop(i);
   
-  irup(sequence[index]);
+  irup(sequence1[index1]);
 }
 
 void irdrop(int i){
-  int pin = irleds[i];
-  digitalWrite(irleds[i], LOW);
+  int pin = irleds1[i];
+  digitalWrite(irleds1[i], LOW);
   Serial.print("[ir ");
   Serial.print(pin);
   Serial.println(" is down]");
 }
 
 void irup(int i){
-  int pin = irleds[i];
-  digitalWrite(irleds[i], HIGH);
+  int pin = irleds1[i];
+  digitalWrite(irleds1[i], HIGH);
   Serial.print("[ir ");
   Serial.print(pin);
   Serial.println(" is up]");
 }
 
 void uvblink(int i){
-  digitalWrite(uvleds[i], HIGH);
+  digitalWrite(uvleds1[i], HIGH);
   delay(UV_DELAY);
-  digitalWrite(uvleds[i], LOW);
-}
-
-void win(){
-  Serial.println("[You have won]");
-  while(true)
-    for(int i = 0;i<length;i++){
-      digitalWrite(irleds[i], HIGH);
-      digitalWrite(uvleds[i], HIGH);
-      delay(500);
-      digitalWrite(irleds[i], LOW);
-      digitalWrite(uvleds[i], LOW);
-      delay(500);
-    }
-}
-
-void loop() {
-  int desired = sequence[index];//todo check if out of scope
-  int pressed = getButton();
-    
-  if(pressed == desired){
-    printState(index, desired, pressed);
-    irdrop(sequence[index]);
-  	index++;
-    
-    uvblink(desired);
-    
-    if(index == slength)
-      win();
-    else
-      irup(sequence[index]);
-  }
-  else if(pressed != -1){
-    printState(index, desired, pressed);
-  	start();
-  }
+  digitalWrite(uvleds1[i], LOW);
 }
 
 void printState(int i, int expected, int actual){
-  Serial.print("[index: ");
+  Serial.print("[index1: ");
   Serial.print(i);
   Serial.print(" expected: ");
   Serial.print(expected);
@@ -102,10 +56,172 @@ void printState(int i, int expected, int actual){
   Serial.println("]");
 }
 
-
 int getButton(){
-  for(int i=0; i<length; i++)
-    if(digitalRead(buttons[i]) == LOW)
+  for(int i=0; i<length1; i++)
+    if(digitalRead(buttons1[i]) == LOW)
       return i;
   return -1;
 }
+
+void next1(){
+    irdrop(sequence1[index1]);
+    uvblink(sequence1[index1]);
+  	index1++;
+        
+    if(index1 == slength1)
+      win1();
+    else
+      irup(sequence1[index1]);
+}
+
+void win1(){
+  Serial.println("[Round 1 win!]");
+  won1 = true;
+  start2();
+}
+void lose1(){
+    start1();
+}
+//=== задача 1
+
+//=== задача 2
+
+bool won2 = false;
+
+const int length2 = 3;//количество кнопок
+int buttons2[length2] = {13, A1, A3};//список кнопок
+int irleds2[length2] = {A0, A2, A4};//список ик
+
+int index2 = 0;
+const int slength2 = 3;//длинна правильной последовательности
+int sequence2[slength2, 2] = {{0,1}, {0,2}, {1,2}};//правильная последовательность
+
+void start2(){
+  Serial.println("[Starting the second game]");
+  index2 = 0;
+  won2 = false;
+  
+  for(int i = 0; i<length2; i++)
+    irdrop2(i);
+  
+  irup2(sequence2[index2]);
+}
+
+void irup2(int i){
+  int pin = irleds2[i];
+  digitalWrite(irleds2[i], HIGH);
+  Serial.print("[ir ");
+  Serial.print(pin);
+  Serial.println(" is up]");
+}
+
+void irdrop2(int i){
+  int pin = irleds2[i];
+  digitalWrite(irleds2[i], LOW);
+  Serial.print("[ir ");
+  Serial.print(pin);
+  Serial.println(" is down]");
+}
+
+void printState2(int i, int[] expected, int[] actual){
+  Serial.print("[index1: ");
+  Serial.print(i);
+  Serial.print(" expected: ");
+  Serial.print(expected[0]);
+  Serial.print(" ");
+  Serial.print(expected[1]);
+  Serial.print(" actual: ");
+  Serial.print(actual[0]);
+  Serial.print(" ");
+  Serial.print(actual[1]);
+  Serial.println("]");
+}
+
+int[] getButtons2(){
+    int[2] result = {-1, -1};
+    int j=0;
+
+  for(int i=0; i<length2; i++){
+    if(j<2){
+        if(digitalRead(buttons2[i]) == LOW){
+            result[j] = i;
+            j++;
+        }
+    }
+  }
+
+  return j;
+}
+
+bool match2(int[] expected, int[] actual){
+    return (expected[0] == actual[0] && expected[1] == actual[1])
+        || (expected[0] == actual[1] && expected[1] == actual[0]);
+}
+
+void next2(){
+    irdrop2(sequence2[index2]);
+  	index2++;
+        
+    if(index2 == slength2)
+      win2();
+    else
+      irup2(sequence2[index2]);
+}
+
+void win2(){
+  Serial.println("[Round 2 win!]");
+  won2 = true;
+}
+void lose2(){
+    start1();
+}
+//=== задача 2
+
+//=== общее
+void setup() {  
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }  
+  
+  for(int i = 0; i<length1; i++){
+  	pinMode(irleds1[i], OUTPUT);
+  	pinMode(uvleds1[i], OUTPUT);
+  	pinMode(buttons1[i], INPUT_PULLUP);
+  }
+  start1();
+  for(int i = 0; i<length2; i++){
+  	pinMode(irleds2[i], OUTPUT);
+  	pinMode(buttons2[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+    if(!won1){
+        int desired = sequence1[index1];
+        int pressed = getButton();
+            
+        if(pressed == desired){
+            printState(index1, desired, pressed);
+            next1();
+        }
+        else if(pressed != -1){
+            printState(index1, desired, pressed);
+            lose1();
+        }
+    }
+    else{        
+        int desired = sequence2[index2];
+        int pressed = getButtons2();
+            
+        if(match2(desired, pressed){
+            printState2(index2, desired, pressed);
+            next2();
+        }
+        else if(pressed != -1){
+            printState2(index2, desired, pressed);
+            lose2();
+        }
+    }
+}
+//=== общее
