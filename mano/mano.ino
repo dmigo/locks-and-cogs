0e866fdcb1b591ed038e9cab25c5ba3be977109e
k0ri0n <burkovwp@gmail.com>
2017-07-19 23:24:33 +0700

    Update mano.ino
    чутка добавил корректировок на тему шумов и фона. так как управляем 220 нагрузкой...

--- mano/mano.ino
+++ mano/mano.ino
@@ -9,6 +9,7 @@
 // License: CC-BY-SA 3.0
 // Downloaded from: https://circuits.io/circuits/3956103-manometer
 
+
 #include <Servo.h>
 
 bool done = false;
@@ -20,21 +21,23 @@ const int servoPin = 5;
 const int servoStart = 50;
 const int servoEnd = 93;
 const int servoStep = 1;
-const int servoSpan = 1000;
+const int servoSpan = 400;
 int servoState = 0;
-
+int start = 200;
+int bak = 0; //ставим переменную "защиты"
 
 Servo servo;
 
 void setup(){
+  
   for(int i = 0; i<4; i++)
     pinMode(switches[i], INPUT_PULLUP);
     
   for(int i = 0; i<7; i++){
     pinMode(relays[i], OUTPUT);//по другому нельзя с реле
-    digitalWrite(relays[i], LOW);    
+    digitalWrite(relays[i], HIGH);    
   }
-  
+  pinMode(A5, INPUT);
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -50,9 +53,9 @@ void moveTo(int newState){
 }
 
 void phase1(){
-  digitalWrite(relays[0], HIGH);
-  digitalWrite(relays[1], HIGH);
-  digitalWrite(relays[2], HIGH);
+  digitalWrite(relays[0], LOW);
+  digitalWrite(relays[1], LOW);
+  digitalWrite(relays[2], LOW);
 
   while(servoState < servoEnd){
     moveTo(servoState + servoStep);
@@ -60,19 +63,24 @@ void phase1(){
   }
 }
 void phase2(){
-  digitalWrite(relays[3], HIGH);
-  digitalWrite(relays[4], HIGH);
-  moveTo(servoStart);  
+  digitalWrite(relays[3], LOW);
+  digitalWrite(relays[4], LOW);
+  moveTo(servoEnd);
+  int time = random(1000, 5000);
+  delay(time);
 }
 void phase3(){
   int time = random(1000, 5000);
   delay(time);
-  digitalWrite(relays[5], HIGH);
+  digitalWrite(relays[5], LOW);
 }
 void phase4(){
-  int time = random(1000, 5000);
+  int time = random(4000, 8000);
   delay(time);
-  digitalWrite(relays[6], HIGH);  
+  digitalWrite(relays[6], LOW);
+  delay(20000);
+  digitalWrite(relays[3], HIGH);
+  digitalWrite(relays[4], HIGH);
 }
 
 bool isSolved(){
@@ -85,7 +93,18 @@ bool isSolved(){
 }
 
 void loop(){
-  if(isSolved() && !done){
+  start = analogRead(A5);  //снимаем наше значение наличия в ааналоге. При стабильном контакте, это даёт 1023 в значении. Но чтобы убрать заранее корозию метала итп. ставим >1000 как верное
+  Serial.println(start);//это для отстройки значений
+  
+  if(start > 1000){
+    bak = bak+1;  //досчитай до 100 чтобы убедиться что это не наводка :)
+     Serial.println(bak);//смотрим, верно ли считает, нет ли пауз при хорошем контакте.
+    }
+    else {
+    bak = 0;//если приходил "левый" сигнал, сбрасываем
+   Serial.println(bak);//и смотрим как сбросили
+    }
+  if(isSolved() && !done && bak > 100){     //проверяем все условия + 100
     phase1();
     phase2();
     phase3();
@@ -93,4 +112,4 @@ void loop(){
 
     done = true;
   }
-}
+ }
