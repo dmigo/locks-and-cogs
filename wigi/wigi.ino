61b516e0852c03e8b47a3548810908193b4b39cf
Dmitry Goryunov <dmitry.goryunov@zalando.de>
2017-11-24 23:29:20 +0100

    Pump version up

--- wigi/wigi.ino
+++ wigi/wigi.ino
@@ -24,7 +24,7 @@ void setup() {
   Serial.begin(9600);
   while (!Serial) {;}
 
-  Serial.println("Version 1.2.4");
+  Serial.println("Version 1.2.5");
   Serial.println("Initializing...");
 
   _rfid = new RfidLock(UID);
